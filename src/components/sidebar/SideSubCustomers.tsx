92bb9c0306fb6f2a54f43f8af842fd5f73410f46
Racheli weiss <rww06560@gmail.com>
2021-07-26 11:32:40 +0300

    add route to sidebar

--- src/components/sidebar/SideSubCustomers.tsx
+++ src/components/sidebar/SideSubCustomers.tsx
@@ -31,7 +31,24 @@ import HomePage from '../HomePage';
         <div>
     
        
-          
+    <Switch>
+           <Route exact path='/home-page'component={HomePage}/>
+            <Route exact path="/customer-search" component={CustomerLocator} />
+            <Route exact path='/customer-details/:userId?' component={CustomerDetails}/>
+            {/* <Route path='/customer-details/:userId' component={CustomerDetails}/> */}
+            <Route exact path='/contact' component={Contact}/> 
+            <Route exact path='/representatives' component={Representatives}/> 
+            <Route exact path='/security' component={Security}/>   
+            <Route exact path='/price-list' component={PriceList}/>    
+            <Route exact path='/obligo' component={Obligo}/> 
+            <Route exact path='/bank-accounts' component={BankAccounts}/> 
+            <Route exact path='/loans' component={Loans}/> 
+            <Route exact path='/documents' component={Documents}/> 
+             <Route exact path='/money-laundering' component={MoneyLaundering}/> 
+            <Route exact path='/concentration-of-actions' component={ConcentrationOfActions}/> 
+            <Route exact path='/log-changes' component={LogChanges}/> 
+            {/* <Route component={Notfound} /> */}
+        </Switch>
      
        
      
